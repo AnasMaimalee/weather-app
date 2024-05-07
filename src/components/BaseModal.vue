f90ad78deede6e6d31224a847d03dccc46e24fcd
maimalee <anasment6@gmail.com>
2024-05-07 22:41:38 +0100

    First Commit

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,14 +1,19 @@
 <template>
-    <div v-show="modalActive"
-        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
-        <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-screen-md">
-            <slot />
+    <Transition name="modal-outer" :duration="{ enter: 500, leave: 800 }">
+        <div v-show="modalActive"
+            class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
+            <Transition name="modal-inner">
+                <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-screen-md">
+                    <slot />
 
-            <button class="text-white mt-8 bg-primary py-2 px-6" @click="$emit('close-modal')">
-                Close
-            </button>
+                    <button class="text-white mt-8 bg-primary py-2 px-6" @click="$emit('close-modal')">
+                        Close
+                    </button>
+                </div>
+            </Transition>
+
         </div>
-    </div>
+    </Transition>
 </template>
 
 <script setup lang="ts">
@@ -20,3 +25,27 @@ defineProps({
     }
 })
 </script>
+
+<style scoped>
+.modal-outer-enter-active {
+    animation: bounce-in 0.5s;
+}
+
+.modal-outer-leave-active {
+    animation: bounce-in 0.5s reverse;
+}
+
+@keyframes bounce-in {
+    0% {
+        transform: scale(0);
+    }
+
+    50% {
+        transform: scale(1.25);
+    }
+
+    100% {
+        transform: scale(1);
+    }
+}
+</style>
