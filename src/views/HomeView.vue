8b9d1f933843e648b7e1933b2b451062f1147051
maimalee <anasment6@gmail.com>
2024-05-08 00:08:19 +0100

    Designing Header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="min-h-screen">
-        <div class="container">
-
+    <main class="container text-white">
+        <div class="pt-4 mb-8 relative">
+            <input type="text" placeholder="Search a City os State"
+                class="py-2 px-1 w-full bg-transparent border-b focus:border-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004EE7]">
         </div>
-    </div>
+    </main>
 </template>
